fa0d7c44df0ddbc7e4e12971ae927d339295f1e0
Bogdan Hatis <bogdan.hatis@outlook.com>
2018-03-19 16:56:23 +0200

    Allow Scripts

--- LandingPage/Areas/Admin/Controllers/AdminController.cs
+++ LandingPage/Areas/Admin/Controllers/AdminController.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LandingPage.Areas.Admin.Controllers
 {
     [Area("Admin")]
     [Route("admin")]
-
+    [Authorize]
     public class AdminController : Controller
     {
         public IActionResult Index()
